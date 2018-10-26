bf49b84c97e996fb955d5d40f9a444e7a07cfb56
Jose Gil Ramirez Sanchez <josegil_ramirez@sps.com.mx>
2018-10-26 00:20:19 -0500

    objetos iniciales

---
+++ BeerApp/BeerApp/Services/RESTClient.swift
@@ -0,0 +1,9 @@
+//
+//  RESTClient.swift
+//  BeerApp
+//
+//  Created by José Gil Ramírez S on 26/10/18.
+//  Copyright © 2018 Los De Atras Hasta Atras. All rights reserved.
+//
+
+import Foundation
